296e26f17c76f8681c6d86bb55528b53a0e7f03d
mtake986 <tosa9629@gmail.com>
2023-09-15 08:42:56 -0600

    9/15 morning

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,13 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(end) {
+  const fizzBuzzArr = [];
+  for (let i = 1; i < end + 1; i++) {
+    if (i % 15 === 0) fizzBuzzArr.push("FizzBuzz");
+    else if (i % 5 === 0) fizzBuzzArr.push("Buzz");
+    else if (i % 3 === 0) fizzBuzzArr.push("Fizz");
+    else fizzBuzzArr.push(i);
+  }
+
+  return fizzBuzzArr;
+}
 
 module.exports = fizzBuzzArray;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,14 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const set = new Set(arr1);
+  const intersectionArr = [];
+
+  for (let i = 0; i < arr2.length; i++) {
+    const num = arr2[i];
+    if (set.has(num)){
+      intersectionArr.push(num)
+    }
+  }
+  return intersectionArr;
+}
 
 module.exports = arrayIntersection;

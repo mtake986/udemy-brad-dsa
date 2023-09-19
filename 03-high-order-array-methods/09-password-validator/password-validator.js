254ad3f703737f406594e32e3a3d17f9cf099f33
mtake986 <tosa9629@gmail.com>
2023-09-19 11:02:06 -0600

    chl.3, 9-10 done

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,22 @@
-function validatePassword() {}
+function validatePassword(str) {
+  if (str.length < 8) return false;
+
+  const lowercases = "abcdefghijklmnopqrstuvwxyz";
+  const uppercases = lowercases.toUpperCase();
+  const numbers = "1234567890";
+
+  const count = {};
+  str.split('').map((letter) => {
+    lowercases.includes(letter)
+      ? (count["lowercase"] = true)
+      : uppercases.includes(letter)
+      ? (count["uppercase"] = true)
+      : numbers.includes(letter)
+      ? (count["number"] = true)
+      : null;
+  });
+
+  return count["lowercase"] === true && count["uppercase"] === true && count['number'] === true
+}
 
 module.exports = validatePassword;
--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,17 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let alphabets = 'abcdefghijklmnopqrstuvwxyz'
+  alphabets = alphabets + alphabets.toUpperCase()
+  const firstIndex = alphabets.split('').indexOf(arr[0])
+  let missingLetter = ''
+  arr.map((letter, i) => {
+    if (missingLetter === '') {
+      if (alphabets[firstIndex + i] !== letter) {
+        missingLetter = alphabets[firstIndex + i];
+      }
+    }
+  })
+
+  return missingLetter
+}
 
 module.exports = findMissingLetter;
